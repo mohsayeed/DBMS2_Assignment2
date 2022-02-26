e5009bb0f88c9f4eda41c660f75d7461f272c9a3
sayeed <cs19btech11004@iith.ac.in>
2022-02-26 12:34:53 +0530

    pa

--- parser.py
+++ parser.py
@@ -1,4 +1,20 @@
 with open ('source1.txt', 'r') as f:  # Open lorem.txt for reading text
     contents = f.read()              # Read the entire file to a string
 contents = contents.split('\n\n')
-print(contents[0])                           # Print the string
+print(len(contents))
+content1 = contents[0].split('\n')
+print(content1)
+import csv
+
+header = ['title', 'authors', 'year','venue','index','references','abstract']
+data = ['Afghanistan', 652090, 'AF', 'AFG']
+
+
+with open('countries.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write the data
+    writer.writerow(data)
